10a3f025a6a4345e42cc78bd0159cc40bc83c60d
MhmdAwad <mhmdawaddd@gmail.com>
2020-09-26 16:23:51 +0200

    added retrofit

--- app/src/main/java/com/mhmdawad/wallpaperaty/models/UnsplashPhoto.kt
+++ app/src/main/java/com/mhmdawad/wallpaperaty/models/UnsplashPhoto.kt
@@ -3,6 +3,7 @@ package com.mhmdawad.wallpaperaty.models
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
+typealias unsplashPhotos = List<UnsplashPhoto>
 
 @Parcelize
 data class UnsplashPhoto(
---
+++ app/src/main/java/com/mhmdawad/wallpaperaty/source/network/WallpaperApi.kt
@@ -0,0 +1,23 @@
+package com.mhmdawad.wallpaperaty.source.network
+
+import com.mhmdawad.wallpaperaty.models.UnsplashPhoto
+import com.mhmdawad.wallpaperaty.models.unsplashPhotos
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface WallpaperApi {
+
+    companion object {
+        const val BASE_URL = "https://api.unsplash.com/"
+        const val CLIENT_ID = ""
+    }
+
+    @Headers("Accept-Version: v1", "Authorization: Client-ID $CLIENT_ID")
+    @GET("search/photos")
+    suspend fun searchPhotos(
+        @Query("query") query: String,
+        @Query("page") page:Int,
+        @Query("per-page") perPage:Int
+    ): unsplashPhotos
+}
