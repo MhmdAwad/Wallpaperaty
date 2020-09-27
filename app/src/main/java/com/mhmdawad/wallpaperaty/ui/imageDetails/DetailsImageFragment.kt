bee7c7d3a42595fdf4eda60e01b9cbb044505f23
MhmdAwad <mhmdawaddd@gmail.com>
2020-09-27 21:22:22 +0200

    refactored DetailsImageFragment

--- app/src/main/java/com/mhmdawad/wallpaperaty/ui/imageDetails/DetailsImageFragment.kt
+++ app/src/main/java/com/mhmdawad/wallpaperaty/ui/imageDetails/DetailsImageFragment.kt
@@ -1,21 +1,12 @@
 package com.mhmdawad.wallpaperaty.ui.imageDetails
 
-import android.Manifest
-import android.app.AlertDialog
 import android.app.DownloadManager
 import android.app.WallpaperManager
 import android.content.IntentFilter
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
 import android.graphics.drawable.TransitionDrawable
-import android.os.Build
 import android.os.Bundle
 import android.transition.TransitionInflater
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -24,7 +15,6 @@ import com.mhmdawad.wallpaperaty.R
 import com.mhmdawad.wallpaperaty.models.UnsplashPhoto
 import com.mhmdawad.wallpaperaty.utils.*
 import kotlinx.android.synthetic.main.fragment_details_image.*
-import kotlinx.android.synthetic.main.select_set_image_layout.view.*
 
 
 class DetailsImageFragment : Fragment(R.layout.fragment_details_image) {
@@ -48,38 +38,11 @@ class DetailsImageFragment : Fragment(R.layout.fragment_details_image) {
         downloadImageListener()
     }
 
-    private fun checkRunTimePermission() {
-        val writePermission = Manifest.permission.WRITE_EXTERNAL_STORAGE
-        if (ContextCompat.checkSelfPermission(
-                requireContext(),
-                writePermission
-            ) != PackageManager.PERMISSION_GRANTED
-        )
-            requestPermissions(arrayOf(writePermission), 101)
-        else
-            downloadImage()
-    }
 
-    private fun downloadImage() {
-        try {
-            downloadImage(image.urls.regular)
-            Toast.makeText(
-                this.context,
-                "Downloading..",
-                Toast.LENGTH_SHORT
-            ).show()
-        } catch (e: Exception) {
-            Toast.makeText(
-                this.context,
-                "Download Failed!",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-    }
 
     private fun downloadImageListener() {
         downloadFAB.setOnClickListener {
-            checkRunTimePermission()
+            checkPermissionAndDownload(image.urls.regular)
         }
     }
 
@@ -88,35 +51,7 @@ class DetailsImageFragment : Fragment(R.layout.fragment_details_image) {
             val bmpImg = (detailImage.drawable as TransitionDrawable).toBitmap()
             val wallManager =
                 WallpaperManager.getInstance(requireActivity().applicationContext)
-            showCustomDialog(wallManager, bmpImg)
-        }
-    }
-
-    private fun showCustomDialog(
-        wallManager: WallpaperManager,
-        bmpImg: Bitmap
-    ) {
-        val viewGroup: ViewGroup? = view?.findViewById(android.R.id.content)
-        val dialogView: View =
-            LayoutInflater.from(requireContext()).inflate(R.layout.select_set_image_layout, viewGroup, false)
-        val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
-        builder.setView(dialogView)
-        val alertDialog: AlertDialog = builder.create()
-        alertDialog.show()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            dialogView.lockScreenContainer.setOnClickListener {
-                tryRun {
-                    wallManager.setBitmap(bmpImg, null, true, WallpaperManager.FLAG_LOCK)
-                    alertDialog.dismiss()
-                }
-            }
-            dialogView.isVisible = true
-        }
-        dialogView.wallpaperContainer.setOnClickListener {
-            tryRun {
-                wallManager.setBitmap(bmpImg)
-                alertDialog.dismiss()
-            }
+            showSetImageDialog(wallManager, bmpImg)
         }
     }
 
@@ -128,23 +63,29 @@ class DetailsImageFragment : Fragment(R.layout.fragment_details_image) {
         }
     }
 
+    private fun hideAndShowFABs(state: Boolean) {
+        detailImageFAB.setImageResource(
+            if (state)
+                R.drawable.ic_add_24
+            else
+                R.drawable.ic_close_24
+        )
+        downloadContainer.isVisible = !state
+        previewContainer.isVisible = !state
+    }
+
     private fun fabListener() {
         detailImageFAB.setOnClickListener {
-            if (!downloadContainer.isVisible) {
-                detailImageFAB.setImageResource(R.drawable.ic_close_24)
-                downloadContainer.isVisible = true
-                previewContainer.isVisible = true
-            } else {
-                detailImageFAB.setImageResource(R.drawable.ic_add_24)
-                downloadContainer.isVisible = false
-                previewContainer.isVisible = false
-            }
+            hideAndShowFABs(downloadContainer.isVisible)
         }
     }
 
     override fun onResume() {
         super.onResume()
-        requireActivity().registerReceiver(onComplete, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+        requireActivity().registerReceiver(
+            onComplete,
+            IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE)
+        )
     }
 
     override fun onStop() {
--- app/src/main/java/com/mhmdawad/wallpaperaty/utils/Ext.kt
+++ app/src/main/java/com/mhmdawad/wallpaperaty/utils/Ext.kt
@@ -1,16 +1,29 @@
 package com.mhmdawad.wallpaperaty.utils
 
+import android.Manifest
 import android.app.Activity
+import android.app.AlertDialog
 import android.app.DownloadManager
+import android.app.WallpaperManager
 import android.content.*
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
 import android.os.Environment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.mhmdawad.wallpaperaty.R
 import com.mhmdawad.wallpaperaty.ui.NIGHT_MODE
+import kotlinx.android.synthetic.main.select_set_image_layout.view.*
 
 
 fun ImageView.loadImage(imageUrl: String){
@@ -32,14 +45,42 @@ fun Fragment.clearNoLimitFlag(){
         android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
     )
 }
-fun Fragment.downloadImage(url: String){
-    val request = DownloadManager.Request(Uri.parse(url))
-    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_HIDDEN)
-    val nameOfFile = url.split('/')
-    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, nameOfFile[nameOfFile.size-1])
-    val manager =
-        context!!.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-    manager.enqueue(request)
+private fun Fragment.downloadImage(url: String){
+    try {
+        val request = DownloadManager.Request(Uri.parse(url))
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_HIDDEN)
+        val nameOfFile = url.split('/')
+        request.setDestinationInExternalPublicDir(
+            Environment.DIRECTORY_DOWNLOADS,
+            nameOfFile[nameOfFile.size - 1]
+        )
+        val manager =
+            context!!.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        manager.enqueue(request)
+        Toast.makeText(
+            this.context,
+            "Downloading..",
+            Toast.LENGTH_SHORT
+        ).show()
+    }catch (e: Exception) {
+        Toast.makeText(
+            this.context,
+            "Download Failed!",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+}
+
+fun Fragment.checkPermissionAndDownload(imageUrl: String) {
+    val writePermission = Manifest.permission.WRITE_EXTERNAL_STORAGE
+    if (ContextCompat.checkSelfPermission(
+            requireContext(),
+            writePermission
+        ) != PackageManager.PERMISSION_GRANTED
+    )
+        requestPermissions(arrayOf(writePermission), 101)
+    else
+        downloadImage(imageUrl)
 }
 
 fun Fragment.changeMode(mSharedPref: SharedPreferences) {
@@ -72,6 +113,33 @@ fun Fragment.tryRun(func: () -> Unit){
     }
 }
 
+fun Fragment.showSetImageDialog(
+    wallManager: WallpaperManager,
+    bmpImg: Bitmap
+) {
+    val viewGroup: ViewGroup? = view?.findViewById(android.R.id.content)
+    val dialogView: View =
+        LayoutInflater.from(requireContext()).inflate(R.layout.select_set_image_layout, viewGroup, false)
+    val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+    builder.setView(dialogView)
+    val alertDialog: AlertDialog = builder.create()
+    alertDialog.show()
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        dialogView.lockScreenContainer.setOnClickListener {
+            tryRun {
+                wallManager.setBitmap(bmpImg, null, true, WallpaperManager.FLAG_LOCK)
+                alertDialog.dismiss()
+            }
+        }
+        dialogView.isVisible = true
+    }
+    dialogView.wallpaperContainer.setOnClickListener {
+        tryRun {
+            wallManager.setBitmap(bmpImg)
+            alertDialog.dismiss()
+        }
+    }
+}
 var onComplete = object : BroadcastReceiver() {
     override fun onReceive(ctxt: Context, intent: Intent) {
         Toast.makeText(ctxt, "Downloaded", Toast.LENGTH_SHORT).show()
